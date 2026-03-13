7bbd095a2c98dca19956d2ff737854326c92f198
faisal5619 <153396166+faisal5619@users.noreply.github.com>
2026-03-14 00:01:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -630,7 +630,7 @@ def page_dashboard():
 
 
     with right:
-        with st.container(border=True):
+        with st.container():
             st.markdown("<div class='inside-box-title'>Match Score</div>", unsafe_allow_html=True)
 
             if run:
