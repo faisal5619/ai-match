6a31b04abcd5334eb552e39109a99bfecc155caf
faisal5619 <153396166+faisal5619@users.noreply.github.com>
2026-03-14 22:30:47 +0300

    Update sample_jobs.py

--- data/sample_jobs.py
+++ data/sample_jobs.py
@@ -1,1 +1,27 @@
-
+SAMPLE_JOBS = [
+    {
+        "title": "Python Developer",
+        "company": "Tech Solutions",
+        "location": "Abha",
+        "description": """
+        We are looking for a Python Developer with experience in Python, SQL, Git, APIs,
+        teamwork, problem solving, communication, and software development.
+        """,
+        "url": "https://example.com/python-developer",
+        "source": "sample_jobs"
+    },
+    {
+        "title": "AI Engineer",
+        "company": "Smart Vision",
+        "location": "Riyadh",
+        "description": """
+        Looking for an AI Engineer with skills in Python, machine learning, artificial intelligence,
+        NLP, data analysis, algorithms, teamwork, and communication.
+        """,
+        "url": "https://example.com/ai-engineer",
+        "source": "sample_jobs"
+    },
+    {
+        "title": "Frontend Developer",
+        "company": "Web House",
+        "location
